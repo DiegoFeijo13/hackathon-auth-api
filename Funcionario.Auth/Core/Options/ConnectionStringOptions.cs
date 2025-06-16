8edb5e2bc2ea0b38e9ae1aad025114bdb27a7e7a
Diego Feijó <diego.feijo@hotmail.com>
2025-06-16 20:50:39 -0300

    feat: migrations

--- Funcionario.Auth/Core/Options/ConnectionStringOptions.cs
+++ Funcionario.Auth/Core/Options/ConnectionStringOptions.cs
@@ -2,6 +2,6 @@ namespace Core.Options;
 
 public class ConnectionStringOptions
 {
-    public const string SectionName = "ConnectionString";
+    public const string SectionName = "ConnectionStrings";
     public string DefaultConnection { get; set; } = string.Empty;
 }
--- Funcionario.Auth/Infrastructure/Data/AppDbContext.cs
+++ Funcionario.Auth/Infrastructure/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<FuncionarioEntity> Funcionarios {get;set;}
 }
