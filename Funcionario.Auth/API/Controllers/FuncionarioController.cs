e9d7f3d20832b5bd25cbaf9009ff3dfc4c033648
Diego Feijó <diego.feijo@hotmail.com>
2025-06-23 19:39:35 -0300

    tests: adicionado unit tests

--- Funcionario.Auth/API/Controllers/FuncionarioController.cs
+++ Funcionario.Auth/API/Controllers/FuncionarioController.cs
@@ -1,5 +1,6 @@
 using Application.Commands;
-using Domain.Entities;
+using Application.Extensions;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +13,37 @@ public class FuncionarioController(ISender sender) : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CriarFuncionarioAsync([FromBody] CriarFuncionarioCommand funcionario)
     {
-        var result = await sender.Send(funcionario);
-        return Created("", result);
+        try
+        {
+            var result = await sender.Send(funcionario);
+            return Created("", result);
+        }
+        catch(ValidationException ex)
+        {
+            return BadRequest(ex.ToResultMessage());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { Error = "Ocorreu um erro inesperado.", Details = ex.Message });
+        }
+        
     }
     
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletarFuncionario([FromRoute]Guid id)
+    public async Task<IActionResult> DeletarFuncionarioAsync([FromRoute]Guid id)
     {
-        var result = await sender.Send(new DeletarFuncionarioCommand(id));
-        return Ok(result);
+        try
+        {
+            var result = await sender.Send(new DeletarFuncionarioCommand(id));
+            return Ok($"Usuário com id {id} removido com sucesso.");
+        }
+        catch(KeyNotFoundException ex)
+        {
+            return NotFound($"Usuário com id {id} não encontrado.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { Error = "Ocorreu um erro inesperado.", Details = ex.Message });
+        }
     }
 }
---
+++ Funcionario.Auth/UnitTests/API/Funcionario/DeletarFuncionarioTests.cs
@@ -0,0 +1,51 @@
+﻿using API.Controllers;
+using Application.Commands;
+using Core.Enums;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace UnitTests.API.Funcionario;
+public class DeletarFuncionarioTests
+{
+    private readonly Mock<ISender> _senderMock;
+    private readonly FuncionarioController _sut;
+
+    public DeletarFuncionarioTests()
+    {
+        _senderMock = new Mock<ISender>();
+        _sut = new FuncionarioController(_senderMock.Object);
+    }
+
+    [Fact]
+    public async Task DeletarFuncionarioAsync_InformadoIdValido_DeverRetornarOk()
+    {
+        var id = Guid.NewGuid();
+
+        _senderMock
+            .Setup(m => m.Send(It.IsAny<DeletarFuncionarioCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var result = await _sut.DeletarFuncionarioAsync(id);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal($"Usuário com id {id} removido com sucesso.", okResult.Value);
+    }
+
+    [Fact]
+    public async Task DeletarFuncionarioAsync_InformadoIdInvalido_DeveRetornarNotFound()
+    {
+        var id = Guid.NewGuid();
+
+        _senderMock
+            .Setup(m => m.Send(It.IsAny<DeletarFuncionarioCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new KeyNotFoundException());
+
+        var result = await _sut.DeletarFuncionarioAsync(id);
+
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal($"Usuário com id {id} não encontrado.", notFoundResult.Value);
+    }
+}
